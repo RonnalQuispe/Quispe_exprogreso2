2895a5fd0968a047c4193115e45a2a79839aca1f
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-12-06 15:11:58 -0500

    hacemos una matriz de 3x23

--- progreso.c
+++ progreso.c
@@ -3,6 +3,18 @@
 #include <time.h>
 int main(int argc, char const *argv[])
 {
+    int fila=23, columna=3;
+    int i,j;
+    int notasprogreso[fila][columna];
+    for ( i = 0; i < fila; i++)
+    {
+        for (j = 0; i < columna; i++)
+        {
+            notasprogreso[i][j] =rand() % 10+1; // generamos numeros aleatorios entre 1-10 para la matriz notas progreso
+        }
+        
+    }
+
     
     return 0;
 }
