2b5b3055b06d1d6f8fb3fca5677d2c6bf1aa2fba
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-12-06 15:43:50 -0500

    generamos nombres a cada nota del alumno del 1-23 utilizando la variabe-i-

--- progreso.c
+++ progreso.c
@@ -3,18 +3,17 @@
 #include <time.h>
 int main(int argc, char const *argv[])
 {
-    int fila=23, columna=3;
-    int i,j;
-    int numalumnos=1;//variable para generar numeros del 1-23
+    int fila = 23, columna = 3;
+    int i, j;
+    
     int notasprogreso[fila][columna];
-    srand(time(NULL));// agregamos para que se puedan generar los numeros aleatorios del 0s-10
-    for ( i = 0; i < fila; i++)
+    srand(time(NULL)); // agregamos para que se puedan generar los numeros aleatorios del 0s-10
+    for (i = 0; i < fila; i++)
     {
         for (j = 0; j < columna; j++)
         {
-            notasprogreso[i][j] =rand() % 11; // generamos numeros aleatorios entre 0-10 para la matriz notas progreso
+            notasprogreso[i][j] = rand() % 11; // generamos numeros aleatorios entre 0-10 para la matriz notas progreso
         }
-        
     }
     printf("\n");
     printf("La  primera columna es la nota del primer progreso\n");
@@ -23,15 +22,29 @@ int main(int argc, char const *argv[])
     printf("\n");
     for (i = 0; i < fila; i++)
     {
-        
-        
+        printf("notas del ESTUDIANTE %d:", i+1);//generamos el nobre o numero del estudiante
         for (j = 0; j < columna; j++)
         {
+            
+
             printf("    %d", notasprogreso[i][j]); // imprimimos las notas de progreso
         }
         printf("\n");
-       
+        // Calcular el promedio de la fila
+        int suma = 0;
+        for (j = 0; j < columna; j++)
+        {
+            suma += notasprogreso[i][j];// se suma el numero de columnas 
+        }
+        float promedio = (float)suma / columna;//calculamos el promediodividiendo suma entre el numero de colmnas 
+
+        printf("****Promedio del estudiante %d: %.2f ****", i + 1, promedio); // Imprimir el promedio de la fila con dos decimales  expresado en "%.2f"
+
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
     }
-    
+
     return 0;
 }
